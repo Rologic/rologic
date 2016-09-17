fda5104076598ecb7be4c911248823d2d8a1e700
Jesse Reitsma <mail@jessereitsma.nl>
2016-09-17 16:59:23 +0200

    :tada: Add basic example file
    This example file is used as playground to create the module with

---
+++ example/index.js
@@ -0,0 +1,31 @@
+import Rologic from '../index'
+
+const actions = {};
+
+actions.sendMessage = (params) => {
+  console.log('Send message to:', params.$name);
+};
+
+const matching = [
+  {
+    'commands': [
+      'Send $name a message'
+    ],
+    'action': 'sendMessage',
+    'params': {
+      '$name': {
+        'type': 'string',
+        'minLength': 2,
+        'maxLength': 20
+      }
+    }
+  }
+];
+
+const rologic = new Rologic(matching);
+
+rologic.cmd('send Jesse a message').then(res => {
+  actions[res.fn](res.params);
+}).catch(() => {
+  console.log('No matches found :(');
+});
